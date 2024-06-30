abafa72a0d01e82dd22e8208c7e83c847780565c
mll-enh <marcco.laskowski@gmail.com>
2024-06-30 00:12:58 -0400

    test

--- test/assets/stages/hall.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-  --background
-  makeLuaSprite('bg','hall',-195,-70)
-  makeLuaSprite('bg2','waterfall',-300,-175)
-  scaleObject('bg2',1.3,1.3)
-  scaleObject('bg',1.7,1.5)
-  addLuaSprite('bg2',false)
-  addLuaSprite('bg',false)
-end
